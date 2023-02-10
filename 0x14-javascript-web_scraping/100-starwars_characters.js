e9aeeb81ef3f5d72187c1c31aacffa5c52c1a3c2
Reuben Ighodalo Enahoro <107653054+rubbycool@users.noreply.github.com>
2023-02-10 17:27:52 +0100

    Update 100-starwars_characters.js

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,18 +1,14 @@
 #!/usr/bin/node
-// A script that prints all characters of a Star Wars movie
 
 const request = require('request');
-const film = process.argv[2];
-const url = `https://swapi-api.alx-tools.com/api/people/${film}`;
-request(url, (err, res, body) => {
-  if (!err) {
-    const characters = JSON.parse(body).characters;
-    characters.forEach((character) => {
-      request(character, function (err, res, body) {
-        if (!err) {
-          console.log(JSON.parse(body).name);
-        }
-      });
+const starWarsUri = 'https://swapi-api.hbtn.io/api/films/'.concat(process.argv[2]);
+
+request(starWarsUri, function (_err, _res, body) {
+  const characters = JSON.parse(body).characters;
+
+  for (let i = 0; i < characters.length; ++i) {
+    request(characters[i], function (_cErr, _cRes, cBody) {
+      console.log(JSON.parse(cBody).name);
     });
   }
 });
